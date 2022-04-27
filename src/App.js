d66c13aa62292428fe84de88cebdaa5d8e74d474
RYAN OKAL <80028082+Ryanokal@users.noreply.github.com>
2022-04-27 16:47:53 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 var formBtn = document.getElementById("formBtn");
 var picBtn = document.getElementById("picBtn");
 var portBtn = document.getElementById("portBtn");
+var details = document.getElementById("details")
+var pics = document.getElementById("pics");
 
 
 
-formBtn.onclick = function() {
+formBtn.onclick = function () {
+    details.style.left = '0px';
+    details.style.opacity = '1';
+    pics.style.left = '-500px';
+    pics.style.opacity = '0';
+    formBtn.classList.add('active');
+    picBtn.classList.remove('active');
     
+}
+
+picBtn.onclick = function (){
+    pics.style.left = '0px';
+    pics.style.opacity = '1';
+    details.style.opacity = '0';
+    formBtn.classList.remove('active');
+    picBtn.classList.add('active');
+
 }
