0fbdef5becd9ee82d2e08e6f5e929481140b0b63
RYAN OKAL <80028082+Ryanokal@users.noreply.github.com>
2022-04-27 16:06:53 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
 var formBtn = document.getElementById("formBtn");
 var picBtn = document.getElementById("picBtn");
 var portBtn = document.getElementById("portBtn");
+
+
+
+formBtn.onclick = function() {
+    
+}
